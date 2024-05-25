3e1eda7efb195a8d5c83a225cebc5ae981ad2c8a
Divya-Nadkarni <git21cs067-t@students.git.edu>
2024-05-25 19:28:30 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 15;
+export const RES_PER_PAGE = 14;
+export const API_KEY = "c0d54729-9dee-46c4-b25d-a82e8a833781";
+export const MODEL_CLOSE_SEC = 2.5;
